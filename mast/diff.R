201b4c405550d844e6bf9108f462919bd955ed03
David J. H. Shih <djh.shih@gmail.com>
2022-02-08 02:09:12 -0600

    Added plots.

--- mast/diff.R
+++ mast/diff.R
@@ -1,10 +1,14 @@
 library(io)
 library(MAST)
+library(ggplot2)
+library(ggrepel)
+library(dplyr)
 
+source("R/common.R")
 
 options(mc.cores=128);
 
-set.seed(1334);
+set.seed(1337);
 
 fdr.cut <- 0.05;
 
@@ -74,22 +78,155 @@ compare_sets <- function(xs) {
 	table(idx)
 }
 
-sgenes.wd <- list(
-	durable = extract_sig_genes(wd$durable),
-	transient = extract_sig_genes(wd$transient),
-	durable.vs.transient = extract_sig_genes(wd$durable.vs.transient)
-);
-
+sgenes.wd <- lapply(wd, extract_sig_genes);
 compare_sets(sgenes.wd)
 
-sgenes.wd.disc <- list(
-	durable = extract_sig_genes(wd$durable, component="disc"),
-	transient = extract_sig_genes(wd$transient, component="disc"),
-	durable.vs.transient = extract_sig_genes(wd$durable.vs.transient, component="disc")
+sgenes.wd.disc <- lapply(wd, function(x) extract_sig_genes(x, component="disc"));
+compare_sets(sgenes.wd.disc)
+
+gene_logfc <- function(sca, groups, genes=NULL) {
+	group.levels <- levels(groups);
+	names(group.levels) <- group.levels;
+	means <- lapply(group.levels, function(g) {
+		idx <- which(groups == g);
+		if (is.null(genes)) {
+			rowMeans(logcounts(sca)[, idx, drop=FALSE])
+		} else {
+			rowMeans(logcounts(sca)[genes, idx, drop=FALSE])
+		}
+	});
+	means[[2]] - means[[1]]
+}
+
+response.contrasts <- list(
+	durable = c("nonresponsive", "durable"),
+	transient = c("nonresponsive", "transient"),
+	durable.vs.transient = c("transient", "durable")
 );
 
+logfcs <- mapply(
+	function(cont) {
+		gene_logfc(
+			sca,
+			factor(colData(sca)$response, cont)
+		)
+	},
+	response.contrasts,
+	SIMPLIFY = FALSE
+);
 
-spids.lr <- extract_sig_genes(lr, symbol=FALSE);
+wd.ds <- mapply(
+	function(d, logfc) {
+		data.frame(
+			gene = rownames(d),
+			logfc = logfc,
+			discrete_q = p.adjust(d[, "disc", "Pr(>Chisq)"], "BH"),
+			continuous_q = p.adjust(d[, "cont", "Pr(>Chisq)"], "BH"),
+			hurdle_q = p.adjust(d[, "hurdle", "Pr(>Chisq)"], "BH")
+		)
+	},
+	wd,
+	logfcs,	
+	SIMPLIFY = FALSE
+);
+
+wd_results_plot <- function(d, fdr.cut=0.05, n.top=30, n.ns=500) {
+	d$group <- ifelse(
+		d$hurdle_q < fdr.cut,
+		ifelse(d$logfc > 0, "Up", "Down"),
+		"NS"
+	);
+
+	d <- d[order(d$hurdle_q), ];
+	d$label <- NA;
+	d$label[1:n.top] <- d$gene[1:n.top];
+	d$label[d$group == "NS"] <- NA;
+
+	# downsample NS
+	sig.idx <- which(d$group != "NS");
+	prob <- -log(d$hurdle_q);
+	prob[is.na(prob)] <- 0;
+	idx <- union(sig.idx, sample(1:nrow(d), n.ns, prob=prob));
+
+	ggplot(d[idx, ],
+		aes(
+			x=discrete_q, y=continuous_q, colour=group
+		)
+	) +
+		theme_classic() +
+		geom_point(alpha=0.3) +
+		scale_colour_manual(values=c(NS="grey30", Up="firebrick3", Down="royalblue4")) +
+		geom_text_repel(aes(label=label), colour="black", show.legend=FALSE) +
+		scale_x_continuous(trans=revlog_trans(), name="FDR of discrete component") +
+		scale_y_continuous(trans=revlog_trans(), name="FDR of continuous component")
+}
+
+volcano_plot <- function(d, fdr.cut=0.05, n.top=30, n.ns=500) {
+	d$group <- ifelse(
+		d$q < fdr.cut,
+		ifelse(d$logfc > 0, "Up", "Down"),
+		"NS"
+	);
+
+	d <- d[order(d$hurdle_q), ];
+	d$label <- NA;
+	d$label[1:n.top] <- d$gene[1:n.top];
+	d$label[d$group == "NS"] <- NA;
+	
+	# downsample NS
+	sig.idx <- which(d$group != "NS");
+	prob <- -log(d$q);
+	prob[is.na(prob)] <- 0;
+	idx <- union(sig.idx, sample(1:nrow(d), n.ns, prob=prob));
+
+	ggplot(d[idx, ],
+		aes(
+			x=logfc, y=q, colour=group
+		)
+	) +
+		theme_classic() +
+		geom_vline(xintercept=0, colour="grey60") +
+		geom_hline(yintercept=0.05, colour="grey60") +
+		geom_point(, alpha=0.3) +
+		guides(colour="none") +
+		scale_colour_manual(values=c(NS="grey30", Up="firebrick3", Down="royalblue4")) +
+		geom_text_repel(aes(label=label), colour="black", show.legend=FALSE) +
+		scale_alpha_continuous(trans=revlog_trans(), name="FDR") +
+		scale_y_continuous(trans=revlog_trans(), name="FDR") +
+		scale_x_continuous(name="log FC")
+}
+
+sanitize <- function(x) {
+	gsub(".", "-", x, fixed=TRUE)
+}
+
+components <- c("hurdle", "discrete", "continuous");
+
+for (stratum in names(wd.ds)) {
+	for (component in components) {
+		d <- wd.ds[[stratum]];
+		d$q <- d[[sprintf("%s_q", component)]];
+
+		qdraw(
+			volcano_plot(d)
+			,
+			width = 5,
+			file = insert(pdf.fn, c("wd", sanitize(stratum), "volcano", component))
+		);
+	}
+}
+
+for (stratum in names(wd.ds)) {
+	qdraw(
+		wd_results_plot(wd.ds[[stratum]])
+		,
+		width = 6,
+		file = insert(pdf.fn, c("wd", sanitize(stratum), "disc-cont-fdr"))
+	);
+}
+
+
+spids.lr <- extract_sig_genes(lr);
 sgenes.lr <- pid_to_genes(spids.lr);
 qwrite(sgenes.lr, insert(out.fn, c("lr", "sig-genes"), ext="vtr"));
 
